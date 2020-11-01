cf7c1f2213eecbeae46b1377c4a3cd83bd2ed365
Tiffany Choong <tiffany.choong@rbc.com>
2020-10-31 21:22:37 -0400

    🛠 Move svg into its own component

---
+++ src/components/Pokeball.vue
@@ -0,0 +1,35 @@
+<template>
+	<svg class="header-svg" viewBox="0 0 288.26 293.35">
+		<defs>
+			<pattern x="-2076.87" y="-13.92" width="24" height="24" patternUnits="userSpaceOnUse" id="circlez" viewBox="12 -36 24 24" style="overflow:visible;">
+				<rect x="12" y="-36" class="st0" width="24" height="24"/>
+				<circle class="st1" cx="24" cy="-24" r="11.5"/>
+				<circle class="st1" cx="36" cy="-12" r="11.5"/>
+				<circle class="st1" cx="12" cy="-12" r="11.5"/>
+				<circle class="st1" cx="36" cy="-36" r="11.5"/>
+				<circle class="st1" cx="12" cy="-36" r="11.5"/>
+			</pattern>
+		</defs>
+		<pattern  id="circlezAlt" xlink:href="#circlez" patternTransform="matrix(1.1594 0 0 1.1594 909.2112 1087.9282)">
+		</pattern>
+		<path class="st2" d="M1,163.03l287.25-50.58C270.62,37.65,197.42-11.26,120.77,2.24S-7.97,86.7,1,163.03z"/>
+		<circle class="st0" cx="161.82" cy="168.71" r="31.81"/>
+		<g>
+			<circle class="st3" cx="161.25" cy="166.75" r="124.08"/>
+			<circle class="st3" cx="161.25" cy="166.75" r="29.78"/>
+			<circle class="st3" cx="161.25" cy="166.75" r="17.15"/>
+			<g>
+				<path class="st3" d="M131.92,171.91c-0.43-2.45-0.55-4.89-0.38-7.26L38,181.12c0.28,2.38,0.62,4.76,1.04,7.15
+					c0.37,2.09,0.79,4.15,1.25,6.19l93.39-16.44C132.89,176.09,132.29,174.05,131.92,171.91z"/>
+				<path class="st3" d="M281.99,138.16l-93.54,16.47c0.97,2.18,1.69,4.5,2.12,6.96c0.38,2.14,0.51,4.26,0.43,6.34l93.39-16.44
+					c-0.26-2.08-0.57-4.16-0.94-6.25C283.03,142.85,282.54,140.49,281.99,138.16z"/>
+			</g>
+		</g>
+	</svg>
+</template>
+
+<script>
+	export default {
+		name: 'EdPokeball'
+	}
+</script>
